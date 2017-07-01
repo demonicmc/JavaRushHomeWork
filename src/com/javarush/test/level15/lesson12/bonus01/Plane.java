45c0798e85fb9f96e5ed1d04c2f419a3108059ae
set <berserkmc@mail.ru>
2017-07-01 19:19:44 +0300

    15 lvl

---
+++ src/com/javarush/test/level15/lesson12/bonus01/Plane.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+/**
+ * Created by set on 01.07.17.
+ */
+public class Plane implements Flyable
+{
+    public Plane(int countPas)
+    {
+    }
+
+    @Override
+    public void fly()
+    {
+
+    }
+}
