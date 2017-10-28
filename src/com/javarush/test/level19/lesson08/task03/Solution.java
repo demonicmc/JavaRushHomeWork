d93b6608578715a6f8e410a5426660b9e896f455
set <berserkmc@mail.ru>
2017-10-28 20:22:39 +0300

    lvl19

---
+++ src/com/javarush/test/level19/lesson08/task03/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.test.level19.lesson08.task03;
+
+/* Выводим только цифры
+В методе main подмените объект System.out написанной вами ридер-оберткой по аналогии с лекцией
+Ваша ридер-обертка должна выводить только цифры
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток
+Вывести модифицированную строку в консоль.
+
+Пример вывода:
+12345678
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream console = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+
+        testString.printSomething();
+
+        String reg = "[0-9]+";
+        String result = outputStream.toString();
+
+        Pattern p = Pattern.compile(reg);
+        Matcher m = p.matcher(result);
+        String b = "";
+
+        while (m.find()){
+            b =  b + result.substring(m.start(), m.end());
+        }
+        System.setOut(console);
+
+        System.out.println(b);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
