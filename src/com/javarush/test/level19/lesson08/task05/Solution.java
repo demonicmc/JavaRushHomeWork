d79c25724d96bd66a2fb18d1a01d2c2c8ca898f3
set <berserkmc@mail.ru>
2017-10-29 11:40:46 +0300

    lvl19

---
+++ src/com/javarush/test/level19/lesson08/task05/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.test.level19.lesson08.task05;
+
+/* Дублируем текст
+Считайте с консоли имя файла
+В методе main подмените объект System.out написанной вами ридер-оберткой по аналогии с лекцией
+Ваша ридер-обертка должна дублировать вывод всего текста в файл, имя которого вы считали
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток
+Закройте поток файла
+
+Пример вывода на экран:
+it's a text for testing
+
+Пример тела файла:
+it's a text for testing
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream console = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+
+        System.setOut(stream);
+
+        testString.printSomething();
+
+        String str = outputStream.toString();
+        Scanner sc = new Scanner(System.in);
+        try
+        {
+            FileWriter fw = new FileWriter(new File(sc.nextLine()));
+            fw.write(str);
+            fw.close();
+            sc.close();
+
+            System.setOut(console);
+            System.out.println(str);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
